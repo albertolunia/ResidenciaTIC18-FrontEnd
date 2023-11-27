cb15eb0291036e73fb5a82cb66e5070f6f7cdfcd
albertolunia <albertolunia@gmail.com>
2023-11-27 20:20:05 -0300

    Começo exercicio 1

---
+++ Semana4/Exercicio1/script.js
@@ -0,0 +1,10 @@
+class Computador{
+    constructor(nome, processador, memoria, hd){
+        this.nome = nome;
+        this.processador = processador;
+        this.memoria = memoria;
+        this.hd = hd;
+    }
+
+     
+}
