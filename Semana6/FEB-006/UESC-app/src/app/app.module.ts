e2cc60d1dd5ccb803bf80b52a276124dc0772923
albertolunia <albertolunia@gmail.com>
2023-12-17 22:33:24 -0300

    Subindo inicio da atividade 6

--- Semana6/FEB-006/UESC-app/src/app/app.module.ts
+++ Semana6/FEB-006/UESC-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoticiasComponent } from './noticias/noticias.component';
+import { DestaqueComponent } from './destaque/destaque.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ResultadosComponent } from './resultados/resultados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoticiasComponent,
+    DestaqueComponent,
+    ServicosComponent,
+    ResultadosComponent
   ],
   imports: [
     BrowserModule,
