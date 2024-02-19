b2629e17d6090d0253779f5e54dff7868d2090d9
albertolunia <albertolunia@gmail.com>
2024-02-19 15:11:46 -0300

    Inicial commit p013

---
+++ Semana13/P013/src/services/data-base.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, map, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataBaseService implements OnInit{
+
+  loadAtendimento: Atendimento[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  addAtendimento(Atendimento: {nomeDoCliente: string, data: string, hora: string, tipo: string, status: string, descricao: string})
+  {
+    return this.http.post('https://residencia-9c476-default-rtdb.firebaseio.com/posts.json', Atendimento).subscribe(responseData => {console.log(responseData)});
+  }
+
+  getAtendimentos()
+  {
+    return this.http.get<{[key: string]: Atendimento}>('https://residencia-9c476-default-rtdb.firebaseio.com/posts.json')
+    .pipe(map(responseData => {
+      const postArray: Atendimento[] = [];
+      for(const key in responseData)
+      {
+        if(responseData.hasOwnProperty(key))
+        {
+          postArray.push({...responseData[key], id: key});
+        }
+      }
+      return postArray;
+    }
+    ));
+  }
+  
+    
+}
